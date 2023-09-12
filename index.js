599d5758892d2e6e7585652551176f733d9f13ea
+ <+>
2023-09-11 19:44:42 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLengthInt) {
+  let returnSentence = ""
+  if (rideLengthInt < 400) {
+    returnSentence = "This one is on me!"
+  } else if (rideLengthInt >= 400 && rideLengthInt <= 2000) {
+    returnSentence = "That will be twenty bucks."
+  } else if (rideLengthInt > 2000 && rideLengthInt <= 2500) {
+    returnSentence = "I will gladly take your thirty bucks."
+  } else (
+    returnSentence = "No can do."
+  )
+  return returnSentence
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const response  = city === "NYC" ? "Ok, sounds good." : "No go."
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString){
+  let result = ""
+  switch (tipString) {
+    case "generous":
+      result = "Thank you so much."
+      break;
+    case "not as generous":
+      result = "Thank you."
+      break;
+    default:
+      result = "Bye."
+  }
+  return result
 }
